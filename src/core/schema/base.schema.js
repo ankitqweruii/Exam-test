f7ec1c1a8e095edef9913f75aeaa583c0a3c26c9
alokregenesys <164482449+alokregenesys@users.noreply.github.com>
2024-05-25 20:34:25 +0530

    added code

--- src/core/schema/base.schema.js
+++ src/core/schema/base.schema.js
@@ -1,11 +1,11 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema } from "mongoose";
 
 export const BaseSchema = new Schema({
-    created_at: { type: Date, default: new Date() },
-    updated_at: { type: Date, default: new Date() },
-    deleted_at: { type: Date, default: null },
-    created_by: { type: Schema.ObjectId, default: null },
-    updated_by: { type: Schema.ObjectId, default: null },
-    deleted_by: { type: Schema.ObjectId, default: null },
-    active: { type: Boolean, default: true }
+  created_at: { type: Date, default: new Date() },
+  updated_at: { type: Date, default: new Date() },
+  deleted_at: { type: Date, default: null },
+  created_by: { type: Schema.ObjectId, default: null },
+  updated_by: { type: Schema.ObjectId, default: null },
+  deleted_by: { type: Schema.ObjectId, default: null },
+  active: { type: Boolean, default: true },
 });
